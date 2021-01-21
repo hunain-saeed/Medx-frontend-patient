539f16d21a973589c1d35b516e199cb8dee3033b
hunain631 <hunainsaeed631@gmail.com>
2021-01-21 13:11:23 +0500

    change backend url

--- src/apis/api.js
+++ src/apis/api.js
@@ -1,4 +1,6 @@
-const baseURL = "http://localhost:4000/api";
+const baseURL = "https://medx-oab.herokuapp.com/api";
+
+//const baseURL = "http://localhost:4000/api";
 
 export const Register = `${baseURL}/register/`;
 
@@ -8,10 +10,10 @@ export const DocList = `${baseURL}/doctor`;
 
 export const PatProfile = `${baseURL}/patient/profile`;
 
-export const BookApi = `${baseURL}/appointment/`
+export const BookApi = `${baseURL}/appointment/`;
 
 export const CurDoc = `${baseURL}/doctor/`;
 
 export const Shedule = `${baseURL}/schedule/`;
 
-export const AppointList = `${baseURL}/appointment/patient/view`
+export const AppointList = `${baseURL}/appointment/patient/view`;
