4b228d36be6f3fcff08a31a99e6de6fff323e666
hunain631 <hunainsaeed631@gmail.com>
2021-04-26 18:46:28 +0500

    README file created

--- src/components/home/hm/Meettheteam.jsx
+++ src/components/home/hm/Meettheteam.jsx
@@ -1,6 +1,8 @@
+import "./Meettheteam.css";
 import React, { Component } from "react";
 import Card from "react-bootstrap/Card";
-import "./Meettheteam.css";
+import Flip from "react-reveal/Flip";
+
 import saad from "../../../images/saad.jpeg";
 import hunain from "../../../images/hunain.jpeg";
 import kanwal from "../../../images/kanwal.jpeg";
@@ -13,46 +15,52 @@ class Meettheteam extends Component {
         <div className="revheadborder"></div>
         <div className="row m-0">
           <div className="col-md-4 mb-4 d-flex justify-content-center">
-            <Card className="card-size">
-              <Card.Img variant="top" className="card-img" src={saad} />
-              <Card.Body>
-                <Card.Title>Saad Mohammed Khan</Card.Title>
-                <Card.Subtitle>Frontend developer</Card.Subtitle>
-                <Card.Text className="mt-1 text-muted">
-                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
-                  sed dapibus leo nec ornare diam sedasd commodo nibh ante
-                  facilisis bibendum dolor feugiat at."
-                </Card.Text>
-              </Card.Body>
-            </Card>
+            <Flip right>
+              <Card className="card-size">
+                <Card.Img variant="top" className="card-img" src={saad} />
+                <Card.Body>
+                  <Card.Title>Saad Mohammed Khan</Card.Title>
+                  <Card.Subtitle>Frontend developer</Card.Subtitle>
+                  <Card.Text className="mt-1 text-muted">
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Duis sed dapibus leo nec ornare diam sedasd commodo nibh
+                    ante facilisis bibendum dolor feugiat at."
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Flip>
           </div>
           <div className="col-md-4 mb-4 d-flex justify-content-center">
-            <Card className="card-size">
-              <Card.Img variant="top" className="card-img" src={hunain} />
-              <Card.Body>
-                <Card.Title>Huanin Saeed</Card.Title>
-                <Card.Subtitle>MERN Stack developer</Card.Subtitle>
-                <Card.Text className="mt-1 text-muted">
-                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
-                  sed dapibus leo nec ornare diam sedasd commodo nibh ante
-                  facilisis bibendum dolor feugiat at."
-                </Card.Text>
-              </Card.Body>
-            </Card>
+            <Flip top>
+              <Card className="card-size">
+                <Card.Img variant="top" className="card-img" src={hunain} />
+                <Card.Body>
+                  <Card.Title>Huanin Saeed</Card.Title>
+                  <Card.Subtitle>MERN Stack developer</Card.Subtitle>
+                  <Card.Text className="mt-1 text-muted">
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Duis sed dapibus leo nec ornare diam sedasd commodo nibh
+                    ante facilisis bibendum dolor feugiat at."
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Flip>
           </div>
           <div className="col-md-4 mb-4 d-flex justify-content-center">
-            <Card className="card-size">
-              <Card.Img variant="top" className="card-img" src={kanwal} />
-              <Card.Body>
-                <Card.Title>Kanwal Gul</Card.Title>
-                <Card.Subtitle>Backend developer</Card.Subtitle>
-                <Card.Text className="mt-1 text-muted">
-                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
-                  sed dapibus leo nec ornare diam sedasd commodo nibh ante
-                  facilisis bibendum dolor feugiat at."
-                </Card.Text>
-              </Card.Body>
-            </Card>
+            <Flip left>
+              <Card className="card-size">
+                <Card.Img variant="top" className="card-img" src={kanwal} />
+                <Card.Body>
+                  <Card.Title>Kanwal Gul</Card.Title>
+                  <Card.Subtitle>Backend developer</Card.Subtitle>
+                  <Card.Text className="mt-1 text-muted">
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Duis sed dapibus leo nec ornare diam sedasd commodo nibh
+                    ante facilisis bibendum dolor feugiat at."
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Flip>
           </div>
         </div>
       </div>
